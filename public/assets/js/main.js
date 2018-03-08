96c5c0b787b41056bff178b8a0b040aae32823f5
remco <remcohoeneveld@gmail.com>
2018-03-08 09:11:36 +0100

    Added styling changes

---
+++ public/assets/js/main.js
@@ -0,0 +1,25 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Main JS
+/*
+-----------------------------------------------------------------------------------*/
+
+(function($) {
+
+   /*---------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */
+   $(window).load(function() {
+
+      // will first fade out the loading animation
+    	$("#loader").fadeOut("slow", function(){
+        $("#wrapper").css('opacity', '1');
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      });
+
+  	})
+
+
+})(jQuery);
