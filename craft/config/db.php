b54ec0f3b7aa2006029b9eb2661ca7d6cc86734e
remco <remcohoeneveld@gmail.com>
2018-09-20 22:44:35 +0200

    develop: Added new styles and scripts + templates for remcohoeneveld.nl

--- craft/config/db.php
+++ craft/config/db.php
@@ -13,7 +13,7 @@ return array(
 	'server' => 'localhost',
 
 	// The name of the database to select.
-	'database' => 'remcodb',
+	'database' => 'remcohoeneveld',
 
 	// The database username to connect with.
 	'user' => 'root',
