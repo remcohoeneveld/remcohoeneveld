1bbde9d2664415f83b2f3821b74022e72cd9ef62
Remco Hoeneveld <remco@atom.nl>
2020-01-03 15:40:58 +0100

    Fixed styling

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -5749,17 +5749,18 @@ $(document).ready(function () {
 });
 $(window).on("scroll", function () {
 
-    // Add parralax effect.
-    var moving__background = $(".header-background");
-    if ($(this).scrollTop() > 800) {
-        $(".bg-dark").addClass("fixed");
-        $("#content").addClass("pushed");
-    }
-    else {
-        $(".bg-dark").removeClass("fixed");
-        $("#content").removeClass("pushed");
-        moving__background.css({"transform": "translate3d(0px, " + $(this).scrollTop() / 2 + "px, 0px)"});
-    }
+  // Add parralax effect.
+  var moving__background = $(".header-background");
+  var max = 2;
+  if ($(this).scrollTop() > 800) {
+    $(".bg-dark").addClass("fixed");
+    $("#content").addClass("pushed");
+  }
+  else {
+    $(".bg-dark").removeClass("fixed");
+    $("#content").removeClass("pushed");
+    moving__background.css({"transform": "translate3d(0px, " + $(this).scrollTop() / max + "px, 0px)"});
+  }
 });
 $('#contact-form').submit(function (ev) {
   // Prevent the form from actually submitting
--- source/js/combined/site/scroll.js
+++ source/js/combined/site/scroll.js
@@ -1,14 +1,15 @@
 $(window).on("scroll", function () {
 
-    // Add parralax effect.
-    var moving__background = $(".header-background");
-    if ($(this).scrollTop() > 800) {
-        $(".bg-dark").addClass("fixed");
-        $("#content").addClass("pushed");
-    }
-    else {
-        $(".bg-dark").removeClass("fixed");
-        $("#content").removeClass("pushed");
-        moving__background.css({"transform": "translate3d(0px, " + $(this).scrollTop() / 2 + "px, 0px)"});
-    }
+  // Add parralax effect.
+  var moving__background = $(".header-background");
+  var max = 2;
+  if ($(this).scrollTop() > 800) {
+    $(".bg-dark").addClass("fixed");
+    $("#content").addClass("pushed");
+  }
+  else {
+    $(".bg-dark").removeClass("fixed");
+    $("#content").removeClass("pushed");
+    moving__background.css({"transform": "translate3d(0px, " + $(this).scrollTop() / max + "px, 0px)"});
+  }
 });
